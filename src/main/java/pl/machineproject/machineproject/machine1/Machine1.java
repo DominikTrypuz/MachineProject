3573ad9414b36793afab01748e3288f4d7f0d0df
Dominik <DominikTrypuz@gmail.com>
2023-02-09 09:51:56 +0100

    I created class Machine1 and enum ParamValues

---
+++ src/main/java/pl/machineproject/machineproject/machine1/Machine1.java
@@ -0,0 +1,35 @@
+package pl.machineproject.machineproject.machine1;
+
+public class Machine1 {
+
+    private boolean isCorrect(int value, ParamValues paramValues) {
+
+        if (value < 0) {
+            return false;
+        }
+        int maxValue = paramValues.getMaxValue();
+        int minValue = paramValues.getMinValue();
+
+        if (minValue <= value && maxValue >= value) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public void analyzeMachineByParams(int valueOne, int valueTwo, int valueThree) {
+
+        boolean isValueOneCorrect = isCorrect(valueOne, ParamValues.VALUE_ONE);
+        boolean isValueTwoCorrect = isCorrect(valueTwo, ParamValues.VALUE_TWO);
+        boolean isValueThreeCorrect = isCorrect(valueThree, ParamValues.VALUE_THREE);
+
+        if (isValueOneCorrect && isValueTwoCorrect && isValueThreeCorrect) {
+
+            System.out.println("Correct values");
+        } else {
+
+            System.out.println("Incorrect values");
+        }
+    }
+}
