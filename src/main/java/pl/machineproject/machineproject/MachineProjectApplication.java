1a6d2672fe89892d93dfd03a49eea0980c138212
Dominik <DominikTrypuz@gmail.com>
2023-02-09 08:16:12 +0100

    Create new gradle project

---
+++ src/main/java/pl/machineproject/machineproject/MachineProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.machineproject.machineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/machineproject/machineproject/MachineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.machineproject.machineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
